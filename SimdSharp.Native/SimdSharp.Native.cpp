45dff3ccd2dc77768ffa603e224d83a83e2227c5
Bart van der Werf <bluelive@gmail.com>
2020-09-07 17:28:49 +0200

    aliasing between arguments not allowed between reading and writing

--- SimdSharp.Native/SimdSharp.Native.cpp
+++ SimdSharp.Native/SimdSharp.Native.cpp
@@ -12,19 +12,19 @@ float* SSN_CDECL AllocateFloat(int32_t blocks) {
 	return (float*)_aligned_malloc(blocks * SSN_VECTOR_SIZE * sizeof(float), SSN_ALIGNMENT);
 }
 
-void SSN_CDECL ReleaseFloat(float* buffer) {
+void SSN_CDECL ReleaseFloat(float* __restrict buffer) {
 	_aligned_free(buffer);
 }
 
-void SSN_CDECL GetRangeFloat(float* buffer, int32_t offset, float* values, int32_t valuesOffset, int32_t count) {
+void SSN_CDECL GetRangeFloat(float* __restrict buffer, int32_t offset, float* __restrict values, int32_t valuesOffset, int32_t count) {
 	memcpy(&values[valuesOffset], &buffer[offset], count * sizeof(float));
 }
 
-void SSN_CDECL SetRangeFloat(float* buffer, int32_t offset, float* values, int32_t valuesOffset, int32_t count) {
+void SSN_CDECL SetRangeFloat(float* __restrict buffer, int32_t offset, float* __restrict values, int32_t valuesOffset, int32_t count) {
 	memcpy(&buffer[offset], &values[valuesOffset], count * sizeof(float));
 }
 
-void SSN_CDECL SetAllFloat(float* buffer, float value, int32_t blocks) {
+void SSN_CDECL SetAllFloat(float* __restrict buffer, float value, int32_t blocks) {
 	for (int b = 0; b < blocks; ++b) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i)
 			buffer[i] = value;
@@ -32,7 +32,7 @@ void SSN_CDECL SetAllFloat(float* buffer, float value, int32_t blocks) {
 	}
 }
 
-void SSN_CDECL MinFloat(float* buffer, float* min, float* result, int32_t blocks) {
+void SSN_CDECL MinFloat(float* __restrict buffer, float* __restrict min, float* __restrict result, int32_t blocks) {
 	for (int b = 0; b < blocks; ++b) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i) {
 			auto a = buffer[i];
@@ -47,7 +47,7 @@ void SSN_CDECL MinFloat(float* buffer, float* min, float* result, int32_t blocks
 	}
 }
 
-void SSN_CDECL MaxFloat(float* buffer, float* max, float* result, int32_t blocks) {
+void SSN_CDECL MaxFloat(float* __restrict buffer, float* __restrict max, float* __restrict result, int32_t blocks) {
 	for (int b = 0; b < blocks; ++b) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i) {
 			auto a = buffer[i];
@@ -62,7 +62,7 @@ void SSN_CDECL MaxFloat(float* buffer, float* max, float* result, int32_t blocks
 	}
 }
 
-void SSN_CDECL ClampFloat(float* buffer, float* low, float* high, float* result, int32_t blocks) {
+void SSN_CDECL ClampFloat(float* __restrict buffer, float* __restrict low, float* __restrict high, float* __restrict result, int32_t blocks) {
 	for (int b = 0; b < blocks; ++b) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i) {
 			auto a = buffer[i];
@@ -82,7 +82,7 @@ void SSN_CDECL ClampFloat(float* buffer, float* low, float* high, float* result,
 }
 
 
-float SSN_CDECL SumFloat(float* buffer, int blocks) {
+float SSN_CDECL SumFloat(float* __restrict buffer, int blocks) {
 	float result = 0.0f;
 	for (int b = 0; b < blocks; ++b) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i) {
@@ -93,7 +93,7 @@ float SSN_CDECL SumFloat(float* buffer, int blocks) {
 	return result;
 }
 
-void SSN_CDECL AddFloat(float* a, float* b, float* result, int blocks) {
+void SSN_CDECL AddFloat(float* __restrict a, float* __restrict b, float* __restrict result, int blocks) {
 	for (int bi = 0; bi < blocks; ++bi) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i) {
 			result[i] = a[i] + b[i];
@@ -104,7 +104,7 @@ void SSN_CDECL AddFloat(float* a, float* b, float* result, int blocks) {
 	}
 }
 
-void SSN_CDECL SubtractFloat(float* a, float* b, float* result, int blocks) {
+void SSN_CDECL SubtractFloat(float* __restrict a, float* __restrict b, float* __restrict result, int blocks) {
 	for (int bi = 0; bi < blocks; ++bi) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i) {
 			result[i] = a[i] - b[i];
@@ -115,7 +115,7 @@ void SSN_CDECL SubtractFloat(float* a, float* b, float* result, int blocks) {
 	}
 }
 
-void SSN_CDECL MultiplyFloat(float* a, float* b, float* result, int blocks) {
+void SSN_CDECL MultiplyFloat(float* __restrict a, float* __restrict b, float* __restrict result, int blocks) {
 	for (int bi = 0; bi < blocks; ++bi) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i) {
 			result[i] = a[i] * b[i];
@@ -126,7 +126,7 @@ void SSN_CDECL MultiplyFloat(float* a, float* b, float* result, int blocks) {
 	}
 }
 
-void SSN_CDECL DivideFloat(float* a, float* b, float* result, int blocks) {
+void SSN_CDECL DivideFloat(float* __restrict a, float* __restrict b, float* __restrict result, int blocks) {
 	for (int bi = 0; bi < blocks; ++bi) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i) {
 			result[i] = a[i] / b[i];
@@ -137,7 +137,7 @@ void SSN_CDECL DivideFloat(float* a, float* b, float* result, int blocks) {
 	}
 }
 
-void SSN_CDECL MultiplyAddFloat(float* a, float* b, float* c, float* result, int blocks) {
+void SSN_CDECL MultiplyAddFloat(float* __restrict a, float* __restrict b, float* __restrict c, float* __restrict result, int blocks) {
 	for (int bi = 0; bi < blocks; ++bi) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i) {
 			result[i] = a[i] * b[i] + c[i];
@@ -149,7 +149,7 @@ void SSN_CDECL MultiplyAddFloat(float* a, float* b, float* c, float* result, int
 	}
 }
 
-void SSN_CDECL FloorFloat(float* a, float* result, int blocks) {
+void SSN_CDECL FloorFloat(float* __restrict a, float* __restrict result, int blocks) {
 	for (int bi = 0; bi < blocks; ++bi) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i) {
 			result[i] = std::floor(a[i]);
@@ -159,7 +159,7 @@ void SSN_CDECL FloorFloat(float* a, float* result, int blocks) {
 	}
 }
 
-void SSN_CDECL CeilFloat(float* a, float* result, int blocks) {
+void SSN_CDECL CeilFloat(float* __restrict a, float* __restrict result, int blocks) {
 	for (int bi = 0; bi < blocks; ++bi) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i) {
 			result[i] = std::ceil(a[i]);
@@ -169,7 +169,7 @@ void SSN_CDECL CeilFloat(float* a, float* result, int blocks) {
 	}
 }
 
-void SSN_CDECL SelectFloat(float* a, float* b, float* c, float* result, int blocks) {
+void SSN_CDECL SelectFloat(float* __restrict a, float* __restrict b, float* __restrict c, float* __restrict result, int blocks) {
 	for (int bi = 0; bi < blocks; ++bi) {
 		for (int i = 0; i < SSN_VECTOR_SIZE; ++i) {
 			result[i] = a[i] ?  b[i] : c[i];
